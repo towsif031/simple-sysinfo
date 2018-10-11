359acc1e8184071874fc0cb457a0c2be9b300c63
Towsif Ahmed Omi <towsif031@gmail.com>
2018-10-11 23:43:53 +0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+// init win
+let win;
+
+function createwindow(){
+    // Create browser window
+    win = new BrowserWindow({width:800, height:600, icon:__dirname+'/img/simple-sysinfo-icon.png'});
+
+    // Load index.html
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    // Open devtools
+    win.webContents.openDevTools();
+
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+// Run create windows function
+app.on('ready', createwindow);
+
+// Quit when all windows are closed
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') {
+        app.quit();
+    }
+});
